ae6cc4219758e41ca0e43a87b5967af99fab3428
Wade Desir <wededesir@gmail.com>
2021-02-25 11:07:19 -0500

    quarter of

--- Week7.js
+++ Week7.js
@@ -56,3 +56,15 @@ var summation = function (num) {
    }
   return answer
 }
+
+const quarterOf = (month) => {
+  if (month>=1 && month<=3){
+  return 1
+  }else if (month>=4 && month<=6){
+  return 2
+  }else if(month>=7&&month<=9){
+  return 3
+  }else{
+  return 4
+  }
+}
