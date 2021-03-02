2c5dd6b2e124331c53cd420988b08e31af6525b1
Wade Desir <wededesir@gmail.com>
2021-03-02 07:46:40 -0500

    Chuck norris' beard

--- week8.js
+++ week8.js
@@ -18,3 +18,24 @@ function multiplyAll(arr){
 
 //tests
 multiplyAll([1, 2, 3])(2))
+
+/* Hidden within the provided sequence of sequences are numbers that represent the
+letters of the words for some of Chuck's favourite things!
+Your job is to translate them, and return the words so that Chuck can get back to the
+business of punching and kicking things. */
+
+function fistBeard(arr) {
+ //Try and shave him... I dare you!
+  let s = ''
+  arr.forEach( nums => {
+    nums.forEach ( letter => {
+      s = s + String.fromCharCode(letter)
+    })
+  })
+
+  return s
+}
+
+//tests
+fistBeard([[78], [117, 110, 99], [104, 117], [107, 115]]) // 'Nunchuks'
+fistBeard([[70, 97, 99], [101, 45, 75, 105, 99, 107]]) // 'Face-Kick'
