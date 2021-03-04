cbf358e17702c6505bf27c9375e9aed3d755aa47
Wade Desir <wededesir@gmail.com>
2021-03-04 09:14:36 -0500

    Roman Numerals

--- week8.js
+++ week8.js
@@ -93,3 +93,32 @@ fighters = [
 moves = ["right","right","right","right","right","down","left","down","down","up","down","up","left","right","right","up"]
 
 console.log(streetFighterSelection(fighters, [0,0], moves));
+
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+function solution(roman){
+  // complete the solution by transforming the
+  // string roman numeral into an integer
+  let values = { I:1, V:5, X:10, L:50,C:100,D:500,M:1000} //object containing roman numeral values
+  let result = 0
+  let preVal = 0
+  let romanArr = roman.split("")
+  romanArr.forEach( letter => {
+    if (values[letter] > preVal){ //checks if the value being added includes subtraction
+      result -= preVal
+      result += (values[letter] - preVal)
+    }
+    else{
+      result += values[letter]
+    }
+    preVal = values[letter] //stores current value for next time in order to check for subtraction
+  })
+  return result
+}
+
+//tests
+solution('XXI') //should be: 21)
+solution('I') //should be: 1)
+solution('IV') //should be: 4)
+solution('MMVIII') //should be: 2008)
+solution('MDCLXVI') //should be: 1666)
