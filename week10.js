5ce1845f7f3b1fc8cfe92969bea33ff49d53cc70
Wade Desir <wededesir@gmail.com>
2021-03-16 10:08:38 -0400

    Multiples of 3 or 5

--- week10.js
+++ week10.js
@@ -10,3 +10,21 @@ function partsSums(ls) {
   }
   return arr.reverse();
 }
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+*/
+
+function solution(number){
+  let arr = []
+  for(let i = 0; i < number; i++){
+    arr.push(i)
+  }
+
+  return arr.filter(x => x % 3 === 0 || x % 5 === 0).reduce((a,b) => a+ b, 0)
+
+}
