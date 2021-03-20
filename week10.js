6c202b404c55d7f99fda61e21cf8caa120476544
Wade Desir <wededesir@gmail.com>
2021-03-20 10:29:33 -0400

    four/seven

--- week10.js
+++ week10.js
@@ -64,3 +64,23 @@ function race(v1, v2, g) {
   let second = (g * 3600) /( v2 - v1);
   return [Math.trunc(second / 3600), Math.trunc((second % 3600) / 60), Math.trunc(second % 60)]
 }
+
+/*
+Simple kata, simple rules: your function should accept the inputs 4 and 7. If 4 is entered, the function should return 7. If 7 is entered, the function should return 4. Anything else entered as input should return 0. There's only one catch, your function cannot include if statements, switch statements, or the ternary operator (or the eval function due to the fact that you can get around the if requirement using it).
+*/
+
+function fourSeven(n){
+  // Your Code Here
+  let four = 4;
+  let seven = 7
+  let answer = n + 7 - 4 === seven;
+  let answer2 = n + 4 - 7 === four
+  while(answer === true){
+    return seven
+  }
+  while(answer2 === true){
+    return four
+  }
+
+    return 0
+}
