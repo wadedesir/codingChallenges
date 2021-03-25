acf3e8c438ab52cf7493f1ace3a285571a5a3f13
Wade Desir <wededesir@gmail.com>
2021-03-25 00:02:42 -0400

    codewar update

---
+++ week11.js
@@ -0,0 +1,26 @@
+var findMissing = function (list) {
+  let progression = 0
+
+  list.reduce( (acc, number) => {
+    progression = number - acc[acc.length]
+    (progression !== 0) ? acc.push(progression) : console.log(progression)
+
+    acc.forEach( (progNum) => {
+      (progression < progNum) ? console.log(progression) : progression = progNum
+    })
+
+  } [list[0]] )
+
+
+  return progression
+}
+
+console.log(findMissing([1, 3, 4]));
+
+/*
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+*/
+function problem(x){
+  //your code here
+  return isNaN(x) ? 'Error' : x === '' ? 'Error' : x * 50 +6
+}
