514636f1685bda9c57e4015b4fe909b6ec8350d6
Wade Desir <wededesir@gmail.com>
2021-03-26 00:04:03 -0400

    incorrect division

--- week11.js
+++ week11.js
@@ -24,3 +24,8 @@ function problem(x){
   //your code here
   return isNaN(x) ? 'Error' : x === '' ? 'Error' : x * 50 +6
 }
+/*
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+*/
+
+const solve = (x, y) => x / y
