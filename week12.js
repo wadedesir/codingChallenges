870e969c6f4ba0be2dba8fe27cbf84f157a04153
Wade Desir <wededesir@gmail.com>
2021-04-01 16:44:59 -0400

    guess hat color

--- week12.js
+++ week12.js
@@ -24,3 +24,26 @@ var twoSum = function(nums, target) {
 };
 
 console.log(twoSum([3, 2, 4], 6)); //Should return [1, 2]
+
+/*
+Four men, a, b, c and d are standing in a line, one behind another.
+
+There's a wall between the first three people (a, b and c) and the last one (d).
+
+The men a, b and c are lined up in order of height, so that:
+
+person a can see the backs of b and c
+person b can see the back of c
+person c can see just the wall
+There are 4 hats, 2 black and 2 white. Each person is given a hat. None of them can see their own hat, but person a can see the hats of b and c, while person b can see the hat of person c. Neither c nor d can see any hats.
+
+Once a person figures out the color of their hat, they shout it.
+*/
+
+function guessHatColor(a,b,c,d) {
+  //coding and coding..
+  return (b == c) ? 1 : 2 //are b and c's hat the same? if so then 1 will know his hat is different
+  //^otherwise 2 will be able to guess right
+}
+
+console.log(guessHatColor("black","white","black","white")); // should be 2
