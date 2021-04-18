f99df142e219568a539c8beb98e707663826263c
Wade Desir <wededesir@gmail.com>
2021-04-18 11:50:53 -0400

    developers from europe

--- week13.js
+++ week13.js
@@ -58,3 +58,34 @@ function factorial(n){
    }
    return ret
  }
+
+/*
+You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+your function should return number 1.
+
+If, there are no JavaScript developers from Europe then your function should return 0.
+*/
+
+ function countDevelopers(list) {
+  // your awesome code here :)
+  return list.reduce( (acc, object) =>{
+    if (object.continent === "Europe" && object.language === "JavaScript"){
+      acc += 1
+      return acc
+    }else{
+      return acc
+    }
+  }, 0)
+}
