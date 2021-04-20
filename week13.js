86e5da6c69ac88be6fee024c7bcd6274a2510e12
Wade Desir <wededesir@gmail.com>
2021-04-20 12:03:01 -0400

    week 13

--- week13.js
+++ week13.js
@@ -89,3 +89,41 @@ If, there are no JavaScript developers from Europe then your function should ret
     }
   }, 0)
 }
+
+/*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >?
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+your function should return the following array:
+
+[
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+    greeting: 'Hi Madison, what do you like the most about Ruby?'
+  } 
+*/
+
+function greetDevelopers(list) {
+  // thank you for checking out my kata :)
+
+  return list.reduce( (acc, object) =>{
+    object.greeting = `Hi ${object.firstName}, what do you like the most about ${object.language}?`
+    acc.push(object)
+    return acc
+  }, [])
+}
