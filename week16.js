993ae6b2c31427a3c1eb524994b42fdb5b444f12
Wade Desir <wededesir@gmail.com>
2021-04-25 22:09:19 -0400

    fac recursion

--- week16.js
+++ week16.js
@@ -111,3 +111,21 @@ class Dinglemouse{
     }
     
   }
+
+  /*
+  The code is a recursive attempt in calculating the result of a factorial function.
+I.e. the result of 3! (Factorial of 3) is 3x2x1 = 6.
+In case of 0! the result is 1 and in case of a negative factorial the result should be 0.
+
+The goal is to find the bugs in the sourcecode and fix them (play a little pest control).
+*/
+
+  function facRecursion(value){
+    if(value < 0){
+      return 0;
+      }
+    else if(value == 1 || value == 0){
+      return 1;
+    }
+    return facRecursion(value - 1) * value;
+  }
