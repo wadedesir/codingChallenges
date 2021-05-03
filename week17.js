6b1c2764bef0d92140f25be9875260a2f9789fcc
Wade Desir <wededesir@gmail.com>
2021-05-02 21:29:18 -0400

    week 17

--- week17.js
+++ week17.js
@@ -34,3 +34,38 @@ function createArray(number){
     return str.split(' ').reverse().join(' '); // reverse those words
     
   }
+
+/*
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be strings, and normal letters.
+
+For Haskell, body has the type of String and tail has the type of Char. For Go, body has type string and tail has type rune.
+*/
+function correctTail(body, tail){
+  
+    let sub = body[body.length - 1]
+    
+    if (sub == tail){ ;
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  /*
+Expected Behaviour
+Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
+
+But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
+*/
+  function getStatus(isBusy) {
+    var msg = isBusy ? "busy" : "available"
+     
+   return {
+      status: msg
+    }
+  }
