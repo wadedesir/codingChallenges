50e431e911b29c45485426279256976b2b1bf2bb
Wade Desir <wededesir@gmail.com>
2021-05-08 23:20:07 -0400

    dinglemouse

--- week18.js
+++ week18.js
@@ -18,3 +18,65 @@ class Warrior {
     }
 
 }
+
+/*
+The code provided has a method hello which is supposed to show only those attributes which have been explicitly set. Furthermore, it is supposed to say them in the same order they were set.
+
+But it's not working properly.
+*/
+
+class Dinglemouse {
+
+    constructor() { //set initial variables
+        this.name = this.name || '' 
+        this.age = this.age || ''
+        this.sex = this.sex || ''
+        
+        this.retStr = 'Hello.' 
+    }
+
+    setAge(age) {
+        if(this.retStr.indexOf(this.age)){ //if age already exists, replace, otherwise create
+          this.retStr = this.retStr.replace(this.age, age)
+          this.age = age
+        }else{
+          this.age = age
+          this.retStr += ` I am ${this.age}.`
+        }
+
+        return this
+    }
+
+    setSex(sex) { //if sex already exists, replace, otherwise create
+      
+        if(this.retStr.indexOf(this.sex)){
+          this.retStr = this.retStr.replace(this.sex, sex == 'M' ? "male" : "female")
+          this.sex = sex == 'M' ? "male" : "female"
+        }else{
+          this.sex = sex == 'M' ? "male" : "female"
+          this.retStr += ` I am ${this.sex}.`
+        }
+      
+        return this
+    }
+
+    setName(name) { //if name already exists, replace, otherwise create
+        if(this.retStr.indexOf(this.name)){
+          this.retStr = this.retStr.replace(this.name, name)
+          this.name = name
+        }else{
+          this.name = name
+          this.retStr += ` My name is ${this.name}.`
+        }
+
+        return this
+    }
+
+    hello() {
+        return this.retStr
+    }
+
+}
+
+    let dm = new Dinglemouse().setName("Bob").setAge(27).setSex('M')
+    let expected = "Hello. My name is Bob. I am 27. I am male."
