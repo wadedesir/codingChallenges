50f27fd198a06237e9fa709f411c6712d9aa96ac
Wade Desir <wededesir@gmail.com>
2021-05-22 00:34:31 -0400

    mystery, calculate total

--- week19.js
+++ week19.js
@@ -91,3 +91,23 @@ function handOutGift(name) {
   return gifted.includes(name) ? x() : gifted.push(name)
 }
   
+/*
+Create a function that returns the total of a meal including tip and tax. You should not tip on the tax.
+
+You will be given the subtotal, the tax as a percentage and the tip as a percentage. Please round your result to two decimal places.
+*/
+function calculate_total(subtotal, tax, tip) {
+    // Your code here
+      let calcTax = (subtotal * tax) / 100
+      let calcTip = (subtotal * tip) / 100
+    
+      return Math.round(100* (subtotal + calcTip + calcTax))/100
+    }
+
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+function mystery() {
+    var results = {sanity: 'Hello'};
+    return results;
+}
